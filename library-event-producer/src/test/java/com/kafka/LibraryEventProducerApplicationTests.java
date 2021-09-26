eb64d332c66d6813447363f8125d27ce7ba2640e
YOG3SHWAR <yogeshwar.chaturvedi@publicissapient.com>
2021-09-26 13:29:10 +0530

    created spring project for event producer

--- library-event-producer/src/test/java/com/kafka/LibraryEventProducerApplicationTests.java
+++ library-event-producer/src/test/java/com/kafka/LibraryEventProducerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.kafka;
+package com.kafka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkaApplicationTests {
+class LibraryEventProducerApplicationTests {
 
 	@Test
 	void contextLoads() {
