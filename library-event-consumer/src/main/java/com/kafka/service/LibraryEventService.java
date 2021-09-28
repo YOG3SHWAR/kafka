9cc79d1d836885e18303d1c5812927e1bb299571
YOG3SHWAR <kanhay8@gmail.com>
2021-09-28 11:04:38 +0530

    added recovery method

--- library-event-consumer/src/main/java/com/kafka/service/LibraryEventService.java
+++ library-event-consumer/src/main/java/com/kafka/service/LibraryEventService.java
@@ -11,7 +11,10 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,8 +29,8 @@ public class LibraryEventService {
     LibraryEventRepository repository;
 
     @Autowired
-    KafkaTemplate KafkaTemplate;
- 
+    KafkaTemplate<Integer, String> kafkaTemplate;
+
     public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
 
         LibraryEvent libraryEvent = mapper.readValue(consumerRecord.value(), LibraryEvent.class);
@@ -68,7 +71,39 @@ public class LibraryEventService {
         log.info("Successfully saved in db");
     }
 
-    public void handleRecovery(ConsumerRecord<Integer, String> record) {
+    public void handleRecovery(ConsumerRecord<Integer, String> consumerRecord) {
+        Integer key = consumerRecord.key();
+        String value = consumerRecord.value();
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.sendDefault(key, value);
+        // sendDefault will send msg to default topic (configured in application.yml)
+        listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
 
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, value, ex);
+            }
+
+        });
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+
+        log.info("Message sent successfully, key = {}, value = {}, partition = {}", key, value,
+                result.getRecordMetadata().partition());
+
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error sending the message , key = {}, value = {}, message = {}", key, value, ex.getMessage());
+        try {
+            throw ex;
+        } catch (Throwable e) {
+            log.error("Error in onFailure: {}", e.getMessage());
+        }
     }
 }
